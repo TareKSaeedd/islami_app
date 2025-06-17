d0c0a4d6fc0d8d6825447d924dbf4009b52bb3f3
Tarek <Tareksaeed11111@gmail.com>
2025-06-17 16:11:55 +0300

    Initializing islami_app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/screens/home/home_screen.dart';
+
+void main() {
+  runApp(const IslamiApp());
+}
+
+class IslamiApp extends StatelessWidget {
+  const IslamiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+    );
+  }
+}
